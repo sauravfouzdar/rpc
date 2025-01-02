e23beec1057e7891f2d1f7f5e2a13f79b5d27eb0
Saurav Fouzdar <sauravfouzdar@gmail.com>
2025-01-02 11:47:09 +0530

    code cleanup

--- client/python_client.py
+++ client/python_client.py
@@ -5,7 +5,7 @@ import sum_pb2
 import sum_pb2_grpc
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('localhost:1234')
     stub = sum_pb2_grpc.SumServiceStub(channel)
     response = stub.Sum(sum_pb2.SumRequest(a=1, b=2))
     print(response.result)
